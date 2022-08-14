06933dd8846f6b2e2ee726a7a4d8f46a7876696a
unknown <you@example.com>
2022-08-14 06:22:11 -0700

    Programm class has been completed

---
+++ ConsoleApp1/ConsoleApp1/DataBase/Models/User.cs
@@ -0,0 +1,40 @@
+﻿using ConsoleApp1.DataBase.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.DataBase.Models
+{
+    public class User : Entity<int>
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        
+        public DateTime RigisterTime { get; set; } = DateTime.Now;
+        public User(string name, string lastName, string email, string password, int? id = null)
+        {
+            Name = name;
+            LastName = lastName;
+           
+            Email = email;
+            Password = password;
+            if (id != null)
+            {
+                Id = id.Value;
+            }
+            else if (id == null)
+            {
+
+
+            }
+        }
+        public virtual string GetInfo()
+        {
+            return Name + " " + LastName + " " + Email + " "  + Password;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/UI/Program.cs
@@ -0,0 +1,53 @@
+﻿using ConsoleApp1.AppLogic;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1.UI
+{
+    internal class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("/Register");
+            Console.WriteLine("/Login");
+            Console.WriteLine("/exit");
+            Console.WriteLine("/show-blogs-with-comments");
+            Console.WriteLine("/show-filtered-blogs-with-comments");
+            Console.WriteLine("/find-blog-by-code");
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Please enter command: ");
+                string command = Console.ReadLine();
+                if (command == "/Register")
+                {
+                    Authenfication.Register();
+                }
+                else if (command == "/Login")
+                {
+                    Authenfication.Login();
+                }
+                else if (command == "/exit")
+                {
+                    break;
+                }
+                else if (command == "/show-blogs-with-comments")
+                {
+                    ServicBlog.ShowBlog();
+                }
+                else if (command == "/show-filtered-blogs-with-comments")
+                {
+                    ServicBlog.ShowFilteredBlog();
+                }
+                else if (command== "/find-blog-by-code")
+                {
+                    ServicBlog.FindBlogByCode();
+                }
+                else
+                {
+                    Console.WriteLine("Command is not correct");
+                }
+            }
+        }
+    }
+}
